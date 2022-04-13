aa7b4b93a4f39d6b4cdc09b9cf9fd58c2f918953
Er Myril <ermyril@gmail.com>
2022-04-13 12:29:18 +0200

    added emacs doom configuration

---
+++ .doom.d/custom.el
@@ -0,0 +1,29 @@
+;;; custom.el -*- lexical-binding: t; -*-
+
+(defhydra doom-window-resize-hydra (:hint nil)
+  " Resize window with hjkl"
+  ("h" evil-window-decrease-width)
+  ("j" evil-window-increase-height)
+  ("k" evil-window-decrease-height)
+  ("l" evil-window-increase-width)
+  ("q" nil))
+
+
+(map! :leader
+      :desc "Hydra resize"
+      "w SPC" #'doom-window-resize-hydra/body)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
