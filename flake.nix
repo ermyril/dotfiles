b0449b3a879d4fb48ebca38ec39d9dfa97305445
Er Myril <ermyril@gmail.com>
2025-07-07 01:33:39 +0200

    home-manager: enable automatic backups to prevent clobbering

--- flake.nix
+++ flake.nix
@@ -60,6 +60,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
               home-manager.users.ermyril = import ./home-manager/default.nix;
             }
           ];
@@ -74,6 +75,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
               home-manager.users.ermyril = import ./home-manager/default.nix;
             }
           ];
