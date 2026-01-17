ebf8e4eb13010117f68dd9a8ba29c8e6d035ac32
Er Myril <ermyril@gmail.com>
2026-01-18 00:27:33 +0100

    added zfs patch in case if it comes useful

--- hosts/rukako/configuration.nix
+++ hosts/rukako/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [
@@ -20,8 +20,38 @@
 
   networking.hostName = "rukako";
 
-  # Allow Mailcow ports
+  # Allow Mailcow and Caddy ports
   networking.firewall.allowedTCPPorts = [ 22 25 80 443 465 587 143 993 110 995 ];
 
+  environment.systemPackages = with pkgs; [
+    wakeonlan
+  ];
+
+  services.caddy = {
+    enable = true;
+    package = config.nixpkgs.overlays.caddy; # Use default caddy package
+
+    virtualHosts."chat.ermyril.com" = {
+      # This will be the default host
+      hostName = "chat.ermyril.com";
+      extraConfig = ''
+        reverse_proxy 192.168.88.50:11434 {
+            lb_try_duration 60s
+            lb_try_interval 3s
+        }
+      '';
+    };
+  };
+
   system.stateVersion = "25.11";
+
+  nixpkgs.overlays = [
+    (self: super: {
+      caddy = super.caddy.override {
+        plugins = with super.caddy-plugins; [
+          exec
+        ];
+      };
+    })
+  ];
 }
